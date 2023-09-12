3c17a1416731320e0131f99b8b656e0548677d45
gnomedevreact <gnomedevreact@gmail.com>
2023-09-12 18:13:37 +0300

    patch env

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("/api");
   app.enableCors({
-    origin: "*",
+    origin: "https://teriyaki-frontend.vercel.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
